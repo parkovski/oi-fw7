baf875aee9a8fa6538b75fd3ab77c42b418410f2
Parker Snell <parkersnell@gmail.com>
2023-12-21 16:56:36 -0700

    Add typescript

--- src/js/fetch.ts
+++ src/js/fetch.ts
@@ -1,10 +1,12 @@
-export function fetchAny(resource, options) {
+declare var process: any;
+
+export function fetchAny(resource: string, options?: RequestInit): Promise<Response> {
   if (!options) {
     options = { credentials: 'include' };
   } else if (!options.credentials) {
     options.credentials = 'include';
   }
-  let url;
+  let url: string;
   if (process.env.NODE_ENV === 'production') {
     url = `https://api.openinvite.parkovski.com${resource}`;
   } else {
@@ -13,10 +15,10 @@ export function fetchAny(resource, options) {
   return fetch(url, options);
 }
 
-export function fetchText(resource, options) {
+export function fetchText(resource: string, options?: RequestInit): Promise<string> {
   return fetchAny(resource, options).then(x => x.text()).catch(e => { throw e; });
 }
 
-export function fetchJson(resource, options) {
+export function fetchJson(resource: string, options?: RequestInit): Promise<any> {
   return fetchAny(resource, options).then(x => x.json()).catch(e => { throw e; });
 }
