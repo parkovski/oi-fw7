c99975cd08d4df9bbd7ee23be6f4d4bb75fee150
Parker Snell <parkersnell@gmail.com>
2024-01-04 08:38:05 -0700

    fix chat notifications

--- src/services/chat.ts
+++ src/services/chat.ts
@@ -44,25 +44,48 @@ class ChatService {
   constructor() {
     this._chatSummary = new Entity(() => fetchJson('/chat'));
 
-    this.messageReceived(msg => {
-      if (!this._chatSummary.data) {
-        return;
-      }
-      const index = this._chatSummary.data.findIndex(c => c.uid === msg.from);
-      if (index === -1) {
-        this._chatSummary.data.push({
-          uid: msg.from,
-          name: msg.fromName,
-          username: 'undefined',
-          unread: 1,
-        });
-      } else if (this._chatSummary.data[index].unread) {
-        ++this._chatSummary.data[index].unread!;
-      } else {
-        this._chatSummary.data[index].unread = 1;
-      }
+    this.messageReceived(msg => this._newMessage(msg));
+  }
+
+  _newMessage(msg: IncomingUserChatMessage) {
+    if (!this._chatSummary.data) {
+      this._chatSummary.refresh();
+      return;
+    }
+
+    const index = this._chatSummary.data.findIndex(c => c.uid === msg.from);
+    if (index === -1) {
+      this._chatSummary.data.push({
+        uid: msg.from,
+        name: msg.fromName,
+        username: 'undefined',
+        unread: 1,
+      });
+    } else if (this._chatSummary.data[index].unread) {
+      ++this._chatSummary.data[index].unread!;
+    } else {
+      this._chatSummary.data[index].unread = 1;
+    }
+    this._chatSummary.publish();
+  }
+
+  _markMessageRead(uid: string, count: number) {
+    if (!this._chatSummary.data) {
+      this._chatSummary.refresh();
+      return;
+    }
+
+    const index = this._chatSummary.data.findIndex(c => c.uid === uid);
+    if (index === -1) {
+      this._chatSummary.refresh();
+      return;
+    }
+
+    const summary = this._chatSummary.data[index];
+    if (summary.unread !== undefined) {
+      summary.unread -= count;
       this._chatSummary.publish();
-    });
+    }
   }
 
   getChat(id: string) {
@@ -88,22 +111,7 @@ class ChatService {
     }
     webSocketService.sendJson(msg);
 
-    if (this._chatSummary.data) {
-      const summaries = this._chatSummary.data;
-      const index = summaries.findIndex(c => c.uid === uidFrom);
-      if (index !== -1) {
-        const count = Array.isArray(id) ? id.length : 1;
-        if (summaries[index].unread !== undefined) {
-          summaries[index].unread! -= count;
-          if (summaries[index].unread! < 0) {
-            summaries[index].unread = 0;
-          }
-        }
-        this._chatSummary.publish();
-      }
-    } else {
-      this._chatSummary.refresh();
-    }
+    this._markMessageRead(uidFrom, Array.isArray(id) ? id.length : 1);
   }
 
   send(msg: UserChatMessage) {
