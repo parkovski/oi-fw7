dab8cbc25cbc19ed4e24de5abfb8875d81de4f83
Parker Snell <parkersnell@gmail.com>
2024-01-08 09:54:40 -0700

    change push notification title to OpenInvite

--- mobile/src/js/service-worker.js
+++ mobile/src/js/service-worker.js
@@ -4,7 +4,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
 
 self.addEventListener('push', e => {
-  const promiseChain = self.registration.showNotification('Push notification', {
+  const promiseChain = self.registration.showNotification('OpenInvite', {
     body: e.data.text(),
   });
   // Need to do this so the service worker stays active.
