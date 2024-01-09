7a4583824e01e9c956da32652776e4c18138d6d7
Parker Snell <parkersnell@gmail.com>
2024-01-08 19:38:15 -0700

    add some notification actions

--- mobile/src/service-worker.js
+++ mobile/src/service-worker.js
@@ -7,14 +7,38 @@ workbox.precaching.precacheAndRoute(__manifest__);
 self.addEventListener('push', e => {
   const data = e.data.json();
 
-  let body = '(unknown message type)';
-  if (data.m === 'chat') {
-    body = data.fromName + ': ' + data.text;
+  let options;
+  switch (data.m) {
+  case 'chat':
+    options = {
+      body: data.fromName + ': ' + data.text,
+      actions: [
+        {
+          action: 'dismiss',
+          type: 'button',
+          title: 'Dismiss',
+        },
+        {
+          action: 'reply',
+          type: 'text',
+          title: 'Reply',
+          placeholder: 'Type a response...',
+        },
+      ],
+    };
+    break;
+  default:
+    options = { body: '(unknown message type)' };
   }
 
-  const promiseChain = self.registration.showNotification('OpenInvite', {
-    body
-  });
+  const promiseChain = self.registration.showNotification('OpenInvite', options);
   // Need to do this so the service worker stays active.
   e.waitUntil(promiseChain);
+});
+
+self.addEventListener('notificationclick', e => {
+  // text in e.reply
+  // action in e.action or null for click on the notification itself
+  e.notification.close();
+  // Still need to do e.waitUntil() if we're doing anything lengthy here.
 });
