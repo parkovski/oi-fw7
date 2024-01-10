66ec7a687a9b80e0e85f15030450a0a40f7ed3e8
Parker Snell <parkersnell@gmail.com>
2024-01-09 17:27:15 -0700

    test - chat send text only

--- mobile/src/components/chat.svelte
+++ mobile/src/components/chat.svelte
@@ -17,13 +17,38 @@
   let isScrolledToBottom = true;
   let myUid;
 
+  function innerTextPolyfill(elem){
+    var sel = window.getSelection();
+
+    // Remember old selection
+    oldRanges = [];
+    for (i=0; i < sel.rangeCount; i++){
+      oldRanges.push(sel.getRangeAt(i));
+    }
+    sel.removeAllRanges();
+
+    // Get elem text
+    elemRange = document.createRange();
+    elemRange.selectNode(elem);
+    sel.addRange(elemRange);
+    text = sel.toString();
+
+    // Restore old selection and return
+    sel.removeAllRanges();
+    for (i=0; i < oldRanges.length; i++){
+      sel.addRange(oldRanges[i]);
+    }
+
+    return text;
+  }
+
   function sendMessage() {
     const instance = textEditor.instance();
-    let text = instance.value;
-    if (text.endsWith('<br>')) {
+    let text = instance.contentEl.innerText || innerTextPolyfill(instance.contentEl);
+    /*if (text.endsWith('<br>')) {
       // ^^ Firefox does this so delete the trailing <br>.
       text = text.substring(0, text.length - 4);
-    }
+    }*/
     if (text === '') {
       return;
     }
