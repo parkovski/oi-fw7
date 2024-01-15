a525c717979dcc166c40c522c63282d0287abee7
Parker Snell <parkersnell@gmail.com>
2024-01-15 13:26:02 -0700

    move the checkmark (again)

---
+++ mobile/src/assets/export-icons.ps1
@@ -0,0 +1,25 @@
+param([switch]$y)
+
+if (-not $y) {
+  echo "Generate first:"
+  echo "- resources\oi-logo.svg"
+  echo "- resources\splash.svg"
+  echo "- assets-src\apple-touch-icon.png (256x256)"
+  echo "Then run export-icons -y"
+  exit
+}
+
+$inkscape = "C:\Program Files\Inkscape\bin\inkscape.com"
+
+& $inkscape -o "..\..\resources\icon.png" -w 1024 -h 1024 oi-logo.svg
+& $inkscape -o "..\..\resources\splash.png" -w 2732 -h 2732 splash.svg
+& $inkscape -o "..\..\assets-src\web-icon.png" -w 512 -h 512 oi-logo.svg
+echo "Not generated: ..\..\assets-src\apple-touch-icon.png (256x256)"
+& $inkscape -o "..\..\public\icons\favicon.png" -w 128 -h 128 oi-logo.svg
+& $inkscape -o "..\..\public\icons\128x128.png" -w 128 -h 128 oi-logo.svg
+& $inkscape -o "..\..\public\icons\144x144.png" -w 144 -h 144 oi-logo.svg
+& $inkscape -o "..\..\public\icons\152x152.png" -w 152 -h 152 oi-logo.svg
+& $inkscape -o "..\..\public\icons\192x192.png" -w 192 -h 192 oi-logo.svg
+& $inkscape -o "..\..\public\icons\256x256.png" -w 256 -h 256 oi-logo.svg
+& $inkscape -o "..\..\public\icons\512x512.png" -w 512 -h 512 oi-logo.svg
+Copy-Item ..\..\assets-src\apple-touch-icon.png ..\..\public\icons\apple-touch-icon.png
