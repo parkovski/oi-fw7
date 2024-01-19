682a7fba1d9edde4a3a35b75ee86627bbcb69893
Parker Snell <parkersnell@gmail.com>
2024-01-19 07:24:18 -0700

    move settings to model

--- server/src/entity/settings.ts
+++ server/src/entity/settings.ts
@@ -4,33 +4,19 @@ import { handleError } from '../util/error.js';
 import {
   validateUuid, validateOneOf, validateBoolean
 } from '../util/validation.js';
-import { NotificationSettings } from 'oi-types/settings';
+import SettingsModel from '../models/settings.js';
 
 export async function getNotificationSettings(req: Request, res: Response) {
+  let client;
   try {
     const session = validateUuid(req.cookies.session, 401);
-    const settings = await getPool().query<NotificationSettings>(
-      `
-      SELECT
-        COALESCE(notification_settings.event_added, TRUE) event_added,
-        COALESCE(notification_settings.event_responded, TRUE) event_responded,
-        COALESCE(notification_settings.event_commented, TRUE) event_commented,
-        COALESCE(notification_settings.event_attendance_changed, TRUE) event_attendance_changed,
-        COALESCE(notification_settings.chat, TRUE) chat,
-        COALESCE(notification_settings.groupchat, TRUE) groupchat,
-        COALESCE(notification_settings.contact_requested, TRUE) contact_requested,
-        COALESCE(notification_settings.contact_added, TRUE) contact_added,
-        COALESCE(notification_settings.contact_request_approved, TRUE) contact_request_approved
-      FROM notification_settings
-      RIGHT JOIN users ON users.id = notification_settings.uid
-      WHERE users.id = (SELECT uid FROM sessions WHERE sesskey = $1)
-      `,
-      [session]
-    );
-    res.json(settings.rows[0]);
+    client = await getPool().connect();
+    const settings = await new SettingsModel(client).getNotificationSettings(session);
+    res.json(settings);
   } catch (e) {
     handleError(e, res);
   } finally {
+    client && client.release();
     res.end();
   }
 }
@@ -47,63 +33,34 @@ const allowedSettings = [
   'contact_request_approved',
 ];
 export async function setNotificationSetting(req: Request, res: Response) {
+  let client;
   try {
     const session = validateUuid(req.cookies.session, 401);
     const settingName = validateOneOf(req.body.setting, allowedSettings);
     const settingValue = validateBoolean(req.body.value);
-    await getPool().query(
-      `
-      INSERT INTO notification_settings (${settingName}, uid)
-      VALUES ($1, (SELECT uid FROM sessions WHERE sesskey = $2))
-      ON CONFLICT (uid) DO UPDATE SET ${settingName} = $1
-      `,
-      [settingValue, session]
-    );
+
+    client = await getPool().connect();
+
+    await new SettingsModel(client).setNotificationSetting(session, settingName, settingValue);
   } catch (e) {
     handleError(e, res);
   } finally {
+    client && client.release();
     res.end();
   }
 }
 
 export async function setAllNotificationSettings(req: Request, res: Response) {
+  let client;
   try {
     const session = validateUuid(req.cookies.session, 401);
     const value = validateBoolean(req.body.value);
-    await getPool().query(
-      `
-      INSERT INTO notification_settings (
-        uid,
-        event_added,
-        event_responded,
-        event_commented,
-        event_attendance_changed,
-        chat,
-        groupchat,
-        contact_requested,
-        contact_added,
-        contact_request_approved
-      )
-      VALUES (
-        (SELECT uid FROM sessions WHERE sesskey = $1),
-        $2, $2, $2, $2, $2, $2, $2, $2, $2
-      )
-      ON CONFLICT (uid) DO UPDATE SET
-        event_added = $2,
-        event_responded = $2,
-        event_commented = $2,
-        event_attendance_changed = $2,
-        chat = $2,
-        groupchat = $2,
-        contact_requested = $2,
-        contact_added = $2,
-        contact_request_approved = $2
-      `,
-      [session, value]
-    );
+    client = await getPool().connect();
+    await new SettingsModel(client).setAllNotificationSettings(session, value);
   } catch (e) {
     handleError(e, res);
   } finally {
+    client && client.release();
     res.end();
   }
 }
---
+++ server/src/models/settings.ts
@@ -0,0 +1,75 @@
+import DataModel from './data.js';
+import { StatusError } from '../util/error.js';
+import { NotificationSettings } from 'oi-types/settings';
+
+export default class SettingsModel extends DataModel {
+  async getNotificationSettings(session: string): Promise<NotificationSettings> {
+    const result = await this._dbclient.query(
+      `
+      SELECT
+        COALESCE(notification_settings.event_added, TRUE) event_added,
+        COALESCE(notification_settings.event_responded, TRUE) event_responded,
+        COALESCE(notification_settings.event_commented, TRUE) event_commented,
+        COALESCE(notification_settings.event_attendance_changed, TRUE) event_attendance_changed,
+        COALESCE(notification_settings.chat, TRUE) chat,
+        COALESCE(notification_settings.groupchat, TRUE) groupchat,
+        COALESCE(notification_settings.contact_requested, TRUE) contact_requested,
+        COALESCE(notification_settings.contact_added, TRUE) contact_added,
+        COALESCE(notification_settings.contact_request_approved, TRUE) contact_request_approved
+      FROM notification_settings
+      RIGHT JOIN users ON users.id = notification_settings.uid
+      WHERE users.id = (SELECT uid FROM sessions WHERE sesskey = $1)
+      `,
+      [session]
+    );
+    if (!result.rowCount) {
+      throw new StatusError(404, 'User not found')
+    }
+    return result.rows[0];
+  }
+
+  async setNotificationSetting(session: string, settingName: string, value: boolean) {
+    await this._dbclient.query(
+      `
+      INSERT INTO notification_settings (${settingName}, uid)
+      VALUES ($1, (SELECT uid FROM sessions WHERE sesskey = $2))
+      ON CONFLICT (uid) DO UPDATE SET ${settingName} = $1
+      `,
+      [value, session]
+    );
+  }
+
+  async setAllNotificationSettings(session: string, value: boolean) {
+    await this._dbclient.query(
+      `
+      INSERT INTO notification_settings (
+        uid,
+        event_added,
+        event_responded,
+        event_commented,
+        event_attendance_changed,
+        chat,
+        groupchat,
+        contact_requested,
+        contact_added,
+        contact_request_approved
+      )
+      VALUES (
+        (SELECT uid FROM sessions WHERE sesskey = $1),
+        $2, $2, $2, $2, $2, $2, $2, $2, $2
+      )
+      ON CONFLICT (uid) DO UPDATE SET
+        event_added = $2,
+        event_responded = $2,
+        event_commented = $2,
+        event_attendance_changed = $2,
+        chat = $2,
+        groupchat = $2,
+        contact_requested = $2,
+        contact_added = $2,
+        contact_request_approved = $2
+      `,
+      [session, value]
+    );
+  }
+}
