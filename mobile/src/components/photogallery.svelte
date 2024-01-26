0489545736ea5844a22aadd042ab536883872039
Parker Snell <parkersnell@gmail.com>
2024-01-26 13:41:12 -0700

    clean up photo gallery

--- mobile/src/components/photogallery.svelte
+++ mobile/src/components/photogallery.svelte
@@ -1,7 +1,7 @@
 <script>
   import { onMount } from 'svelte';
   export let urls;
-  export let targetSize = 150;
+  export let targetSize = 160;
 
   let imageContainer;
   let popup;
@@ -24,9 +24,10 @@
       img.onload = function() {
         setSize(img, targetSize, targetSize);
       };
-      img.onclick = function() {
+      img.onclick = function(e) {
         if (popup.children.length) {
           popup.removeChild(popup.children[0]);
+          popup.style.display = 'none';
           return;
         }
         const img = document.createElement('img');
@@ -40,36 +41,45 @@
           popup.style.top = `${(document.documentElement.clientHeight - img.height) / 2 - 8}px`;
         };
         popup.appendChild(img);
+        e.stopPropagation();
       }
       div.appendChild(img);
       imageContainer.appendChild(div);
     });
 
-    popup.onclick = function() {
-      if (popup.children.length) {
+    document.documentElement.addEventListener('click', function() {
+      if (popup && popup.children.length) {
         popup.removeChild(popup.children[0]);
+        popup.style.display = 'none';
       }
-      popup.style.display = 'none';
-    }
+    });
   });
 </script>
 
 <style>
-  :global(#image-container div) {
-    display: inline-block;
+  :global(#image-container > div) {
+    display: inline-flex;
     margin: 4px;
-    width: 150px;
-    height: 150px;
-    text-align: center;
-    vertical-align: middle;
+    width: 160px;
+    height: 160px;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top;
     cursor: pointer;
+    background: #d0d0d0;
+  }
+  :global(html.dark #image-container > div) {
+    background: #303030;
   }
 
   #popup {
     position: fixed;
     padding: 8px;
-    background: #606060e0;
+    background: #d0d0d0;
     border-radius: 5px;
+  }
+  :global(html.dark #popup) {
+    background: #303030;
   }
 </style>
 
